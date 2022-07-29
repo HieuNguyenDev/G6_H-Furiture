a0229735d5144fd36ba1accc7afb9f778bf092ad
Nguyen Minh Hieu <86197383+HieuNguyenDev@users.noreply.github.com>
2022-07-29 15:36:01 +0700

    Setup project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { FeaturedProductsComponent } from './featured-products/featured-products.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { BannerComponent } from './banner/banner.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    FeaturedProductsComponent,
+    CategoriesComponent,
+    BannerComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
